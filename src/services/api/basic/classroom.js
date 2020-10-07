657968058084d6169dfbcb27ff5b3bda1e7fc296
illuminated <846433723@qq.com>
2020-10-07 13:48:55 +0800

    feat(api): basic api finished

--- src/services/api/basic/classroom.js
+++ src/services/api/basic/classroom.js
@@ -4,33 +4,52 @@
 import base from '../base'; // 导入接口域名列表
 import axios from '../../axios'; // 导入http中创建的axios实例
 const baseUrl=base.root+'/a/classroom'
-// const ruleUrl=baseUrl+'Rule'
+const ruleUrl=baseUrl+'Rule'
 const classroom = {    
     // 查询全部教室的简要信息
     fetchList(params){
-        return axios.get(`${baseUrl}/getDataListWithBuilding.json`,params, {
-            'Content-Type': 'multipart/form-data'});    
+        return axios.get(`${baseUrl}/getDataListWithBuilding.json`,params);    
     },
 
     // 查询指定教室信息，用于教室规则等的教室选择，编辑模板前，先查询模板信息，利用模板id
     fetchBuilding(params){
-        return axios.get(`${baseUrl}/getDataInfo.json`,params, {
-            'Content-Type': 'multipart/form-data'});    
+        return axios.get(`${baseUrl}/getDataInfo.json`,params);    
     },
 
     // 教室新增接口
     saveBuilding(params){
-        return axios.post(`${baseUrl}/saveData.json`,params, {
-            'Content-Type': 'multipart/form-data'});    
+        return axios.post(`${baseUrl}/saveData.json`,params);    
     },
 
     // 教室删除接口
     deleteBuilding(params){
-        return axios.get(`${baseUrl}/deleteData.json`,params, {
-            'Content-Type': 'multipart/form-data'});   
+        return axios.get(`${baseUrl}/deleteData.json`,params);   
     },
 
-    // 
+    // 场地规则列表查看
+    fetchRuleList(params){
+     return axios.get(`${ruleUrl}/getDataListByCurrId.json`,params);   
+    },
+
+    // 场地规则项查看
+    fetchRule(params){
+        return axios.get(`${ruleUrl}/getDataListByCurrId.json`,params)
+    },
+
+    // 场地规则删除
+    deleteRule(params){
+        return axios.post(`${ruleUrl}/deleteDataJoint.json`,params)
+    },
+    
+    // 场地规则新增编辑
+    saveRule(params){
+        return axios.post(`${ruleUrl}/saveDataJoint.json`,params)
+    },
+
+    // 教室树
+    fetchRoomList(params){
+        return axios.get(`${ruleUrl}/getClassroomList.json`,params)
+    }
 }
 
 export default classroom;
---
+++ src/services/api/basic/grade.js
@@ -0,0 +1,45 @@
+/**
+ * grade模块接口列表
+ */
+import base from '../base'; // 导入接口域名列表
+import axios from '../../axios'; // 导入http中创建的axios实例
+const baseUrl=base.root+'/a/admin'
+const gradeUrl=base.root+'/a/grade'
+const grade = {    
+    // 查询全部级部的简要信息
+    fetchList(params){
+        return axios.get(`${baseUrl}/getAdminGradeDataList.json`,params);    
+    },
+
+    // 查询年级的简要信息
+    fetchGradeList(params){
+        return axios.get(`${gradeUrl}/getGradeDataList.json`,params);    
+    },
+
+    // 查询指定年级信息
+    fetchGrade(params){
+        return axios.get(`${gradeUrl}/getGradeDataInfo.json`,params);    
+    },
+
+    // 年级新增接口
+    saveGrade(params){
+        return axios.post(`${gradeUrl}/saveGradeSubjectData.json`,params);    
+    },
+
+    // 年级删除接口
+    deleteGrade(params){
+        return axios.post(`${gradeUrl}/deleteGradeSubByGradeIds.json`,params);   
+    },
+
+    // 年级子课程新增
+    saveGradeSubject(params){
+     return axios.post(`${gradeUrl}/insertSubToGrade.json`,params);   
+    },
+
+    // 年级子课程删除
+    deleteGradeSubject(params){
+        return axios.post(`${gradeUrl}/deleteGradeSubChild.json`,params)
+    },
+}
+
+export default grade;
